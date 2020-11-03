e98c4ee94954e109dd643ef05f8e848952c01c5b
Simon Gautier <gautier.simon@gmail.com>
2020-11-03 17:41:30 +0100

    Routing

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,17 @@ class HelloWorldController
      */
     public function helloWorldAction()
     {
+        return new Response('Hello World !');
+    }
+
+    /**
+     * @Route("hello")
+     * @param Request $request
+     * @return Response
+     */
+    public function helloWorldBisAction(Request $request)
+    {
+        dump($request->query->get('toto'));
         return new Response('Hello World !');
     }
 }
