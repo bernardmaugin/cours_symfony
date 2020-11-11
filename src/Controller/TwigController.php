b9fd772bd54283e200ab291da78af4ceba436ad0
Simon Gautier <gautier.simon@gmail.com>
2020-11-11 18:01:38 +0100

    Twig - display variables

---
+++ src/Controller/TwigController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use App\MySampleClass\MyClass1;
+use App\MySampleClass\MyClass2;
+use App\MySampleClass\MyClass3;
+use App\MySampleClass\MyClass4;
+use App\MySampleClass\MyClass5;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class TwigController
+ * @package App\Controller
+ * @Route("/twig")
+ */
+class TwigController extends AbstractController
+{
+    /**
+     * @Route("/display_variables", name="twig_display_variables")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function displayVariablesAction()
+    {
+        return $this->render('twig/display_variables.html.twig', [
+            'my_variable' => 'My value',
+            'my_variable_array' => ['name' => 'This is the value for the key "name"'],
+            'my_variable1' => new MyClass1(),
+            'my_variable2' => new MyClass2(),
+            'my_variable3' => new MyClass3(),
+            'my_variable4' => new MyClass4(),
+            'my_variable5' => new MyClass5(),
+        ]);
+    }
+}
